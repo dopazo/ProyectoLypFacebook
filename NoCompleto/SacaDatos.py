380615e7b1a8999bf983cc16ee575c7ccb43f205
dopazo <diegoopazo@hotmail.com>
2017-11-21 19:07:51 -0300

    commit con mas posts
    Saca mas posts pero no mas comments de cada post

--- NoCompleto/SacaDatos.py
+++ NoCompleto/SacaDatos.py
@@ -1,9 +1,10 @@
 import requests
 import facebook
 import json
+import datetime
 
 # Su Token, por alguna razon no me deja usar uno de una cuenta temporal de prueba :c
-access_token = 'EAACEdEose0cBABPoFCuODRz05tkrmzXecOSZCx994oKSZAmziUVVDFE4xm02OCTyBkdgZCEOhBmGIteEkRZBBDbkSZCJ8hKfs7sIBt4ur9nVAWejNHk9iGhsdojDNNocmY0CuTIMEtUVHhzjuu1tw8TUMy7QlVdZAZCpzuwXLnymbJbtMJAggMCMZAARM9G5ZAEwZD'
+access_token = 'EAACEdEose0cBALotohafHTnp3qVjjLGpsnsG2Q5tGez0QZApdQkzZCecOPZBoUlC2LZBGwHjvxBRcFT3ScWVkS22Biz8eKZCENZBNzRMLpOkHX9daBPSH6iMbv68BnR3Ena0gRoXGNZAZAZBcZCxB10wuZCHM6mruwSXQ9CgtAYKjmgdEqtGBhShiZAPSwhaiJClKqcZD'
 
 # Facebook user id de la pagina de Guiller
 user_id = "1481491872064849"
@@ -28,18 +29,16 @@ nombre = usuario['name']
 # Esta la encontré util por si hacemos automatico la busqueda de los candidatos(asegurarnos que sea la oficial)
 #es_verificada = graph.get_object(id=user_id, fields='is_verified')
 #####################
-# ESTAS SON LAS IMPORTANTES
-# saca los comentarios de una publicacion de piñera(id=idPiñera_idPost), pero solo de un post dado
-#comentarios = graph.get_object(id="553775568008058_1698259343559669", fields='comments')
-#print(comentarios['comments'])
-
 # Busca las publicaciones del usuario y entrega en un diccionario
 # el id de la publicación, y los comentarios, que a su vez tienen mas "ramificaciones"
 
 #FECHA DESDE LAS PRIMARIAS HASTA EL DIA DE LAS VOTACIONES
 # since=2017-07-02,until=2017-11-19
+# datetime.datetime(aa,mm,dd,hh,mm,ss).timestamp()
+sinceDate=int(datetime.datetime(2017,11,21,15,00,00).timestamp())
+untilDate=int(datetime.datetime(2017,11,21,16,00,00).timestamp())
 
-prueba = graph.get_object(id=user_id, fields='posts{id,comments, message,since=2017-11-15,until=2017-11-19}')
+prueba = graph.get_object(id=user_id, fields='posts{id,comments, message,since=sinceDate,until=untilDate}')
 #print('Encontrados {} posts'.format(len(prueba['posts']['data'])))
 
 #print(prueba['posts']['data'][0]['comments']['data'])
@@ -48,21 +47,27 @@ prueba = graph.get_object(id=user_id, fields='posts{id,comments, message,since=2
 allData = []
 vWhile=0
 vFor=0
+posts = prueba['posts']
 while True:
     try:
         print("vuelta del while")
         vWhile=vWhile+1
-        for dato in prueba['posts']:
-            allData.append(prueba)
+        for dato in posts:
+            allData.append(posts)
             print("vuelta del for")
+            #Sacar mas comments de cada post
+            if 'paging' in posts['data']['comments'] and 'next' in posts['data']['comments']['paging']:
+                posts = requests.get(posts['data']['comments']['paging']['next']).json()
+                print('Encontrados {} comments'.format(len(posts['data']['comments'])))
             vFor=vFor+1
-        if 'paging' in prueba and 'next' in prueba['paging']:
-            prueba = requests.get(prueba['paging']['next']).json()
-            print('Encontrados {} posts'.format(len(prueba['posts']['data'])))
+        #Sacar mas posts
+        if 'paging' in posts and 'next' in posts['paging']:
+            posts = requests.get(posts['paging']['next']).json()
+            print('Encontrados {} posts'.format(len(posts['data'])))
         else:
             break
-    except KeyError:
-        print("ERROR")
+    except KeyError as e:
+        print("ERROR {}".format(str(e)))
         break
 
 print("...vueltas del While: ")
@@ -73,24 +78,6 @@ print(vFor)
 
 with open('data.json', 'w') as outfile:
     json.dump(allData, outfile, indent=4)
-
-
-''''###while True:
-    try:
-        print("vuelta del while")
-        for prueba['posts']['data'] in prueba:#['post']:#['data']:
-            print("vuelta for prueba")
-            with open('data.json', 'a') as outfile: # 'w' sobreescribir, 'a' escribir al final
-                json.dump(prueba, outfile, indent=4)
-        # Attempt to make a request to the next page of data, if it exists.
-        prueba = requests.get(prueba['posts']['data']['paging']['next']).json()
-    except KeyError:
-        # When there are no more pages (['paging']['next']), break from the
-        # loop and end the script.
-        print("error")
-        break
-'''
-
 
 #with open('data.json', 'w') as outfile:
 #    json.dump(prueba, outfile, indent=4)
